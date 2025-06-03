f4e155a5d18ce56c9b0555b02676b9579e52563f
Kevin Bramasta Arvyto Wardhana <kevinbramastaa@gmail.com>
2025-06-04 06:10:35 +0700

    feat(profile): add edit functionality for supervisor profile

--- resources/views/layout/profile/supervisor.blade.php
+++ resources/views/layout/profile/supervisor.blade.php
@@ -1,35 +1,39 @@
-<div class="container mx-auto min-h-screen font-inter">
-    <h1 class="mb-8 text-3xl font-bold text-gray-900">My Profile</h1>
+<x-dashboard.layout gap="30px">
+    <div class="p-6">
+        <h1 class="mb-8 text-3xl font-bold text-gray-900">My Profile</h1>
 
-    {{-- Top Profile Card (using the component) --}}
-    <div class="mb-8">
-        <x-profup.user-profile
-            name="{{ $user->name }}"
-            role="Supervisor"
-            location="Malang, Indonesia"
-            avatarImage="{{ $user->profile_picture }}"
-        />
+        {{-- Top Profile Card (using the component) --}}
+        <div class="mb-8">
+            <x-profup.user-profile
+                name="{{ $user->name }}"
+                role="Supervisor"
+                location="Malang, Indonesia"
+                avatarImage="{{ $user->profile_picture }}"
+            />
+        </div>
+
+        {{-- Personal Information Section --}}
+        <div class="mb-8">
+            <x-profup.personal-information
+                firstName="{{ $user->first_name }}"
+                lastName="{{ $user->last_name  }}"
+                supervisorId="{{ $user->supervisor->nidn }}"
+                emailAddress="{{ $user->email  }}"
+                phoneNumber="{{ $user->phone   }}"
+                major="Information Technology"
+                userId="{{ $user->id  }}"
+            />
+        </div>
+
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
+            <x-profup.expertise-area
+                :expertiseAreas="$user->supervisor->expertise_areas ?? []"
+                :userId="$user->id"
+            />
+            <x-profup.research-interest
+                :researchInterests="$user->supervisor->research_interests ?? [] "
+                :userId="$user->id"
+            />
+        </div>
     </div>
-
-    {{-- Personal Information Section --}}
-    <div class="mb-8">
-        <x-profup.personal-information
-            firstName="{{ $user->first_name }}"
-            lastName="{{ $user->last_name  }}"
-            supervisorId="{{ $user->supervisor->nidn }}"
-            emailAddress="{{ $user->email  }}"
-            phoneNumber="{{ $user->phone   }}"
-            major="Information Technology"
-        />
-    </div>
-
-    <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
-        <x-profup.expertise-area
-            :expertiseAreas="$user->supervisor->expertise_areas ?? []"
-        />
-        <x-profup.research-interest
-            :researchInterests="$user->supervisor->research_interests ?? [] "
-        />
-    </div>
-</div>
-
+</x-dashboard.layout>
